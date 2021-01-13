0e1bcd1cf4549a06bfc48ce7b7bcb8cc2576542e
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-01-13 22:00:41 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12914 level : 3 - Dynamic Programming(DP)

---
+++ programmers/멀리_뛰기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[n+1];
+        if(n==1) return 1;
+        else if(n==2) return 2;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] %= 1234567;
+        }
+        answer = dp[n];
+
+        return answer;
+    }
+}
