a98d44daf6a1507a0d28de80b163eacf3d72a7ed
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-03-28 22:10:55 +0900

    https://www.acmicpc.net/problem/21279 level : gold1 - PriorityQueue 좀 더 말맞은 알고리즘으로 개선

--- acmicpc.net/boj21279.java
+++ acmicpc.net/boj21279.java
@@ -1,57 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class boj21279 {
     public static int N, C;
-    public static ArrayList<index> dia = new ArrayList<>();
+    public static ArrayList<index>[] xIndex;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        for(int n=0; n<N; n++){
+        xIndex = new ArrayList[100005];
+        for(int i=0; i<xIndex.length; i++){
+            xIndex[i] = new ArrayList<>();
+        }
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            long v = Long.parseLong(st.nextToken());
-            dia.add(new index(x,y,v));
+            long value = Long.parseLong(st.nextToken());
+            xIndex[x].add(new index(x, y, value));
         }
-        Collections.sort(dia, new Comparator<index>() {
-            @Override
-            public int compare(index o1, index o2) {
-                if(o1.x==o2.x) return o1.y - o2.y;
-                return o1.x - o2.x;
-            }
-        });
-        int currentC = 0;
-        long currentV = 0;
         long answer = 0;
+        long currentV = 0L;
         PriorityQueue<index> queue = new PriorityQueue<>();
-
-        for(int i=0; i<dia.size(); i++){
-            index now = dia.get(i);
-            currentC = queue.size();
-            if(currentC < C){
-                queue.add(now);
-                currentV += now.value;
+        for(int i=0; i<xIndex.length; i++){
+            if(xIndex[i].isEmpty()) continue;
+            for(int j=0; j<xIndex[i].size(); j++){
+                queue.add(xIndex[i].get(j));
+                currentV += xIndex[i].get(j).value;
             }
-            else{
-                if(queue.peek().y < now.y) continue;
-                else if(queue.peek().y == now.y){
-                    while(queue.peek().y < now.y){
-                        currentV -= queue.poll().value;
-                    }
-                }
-                else{
+            int prevTop = -1;
+            while(!queue.isEmpty() && queue.size()>C){
+                prevTop = queue.peek().y;
+                currentV -= queue.poll().value;
+                while(!queue.isEmpty() && queue.peek().y == prevTop){
                     currentV -= queue.poll().value;
-                    currentV += now.value;
-                    queue.add(now);
                 }
             }
-
-
             answer = Math.max(answer, currentV);
         }
         System.out.println(answer);
@@ -72,4 +61,5 @@ public class boj21279 {
             return o.y - this.y;
         }
     }
+
 }
